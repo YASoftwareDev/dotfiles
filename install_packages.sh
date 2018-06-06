3859f639adbbb739ef63bb7a5c0143b0d92536ad
Gaius Baltar <yasoftwaredev@gmail.com>
2018-06-06 09:05:53 +0200

    updated zsh,tmux, packages configs

--- install_packages.sh
+++ install_packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-APT_PACKAGES="stow git vim-gtk3 curl fortune clang cowsay build-essential python3-dev python-pip screenfetch tmux compiz-plugins-extra clipit"
+APT_PACKAGES="stow git vim-gtk3 curl fortune clang cowsay build-essential python3-dev python-pip python3-venv screenfetch tmux compiz-plugins-extra clipit zsh tig ranger cheat"
 
 SNAP_PACKAGES="rg"
 
-PIP_PACKAGES="pip virtualenv"
+PIP_PACKAGES="pip virtualenv sudocabulary"
 
 echo -e "\e[30;47mInstalling apt packages.\e[0m"
 apt update
@@ -25,3 +25,33 @@ pip install --upgrade $PIP_PACKAGES
 #echo -e "\e[30;47mInstalling Pathogen.\e[0m"
 #mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 #curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# zsh and customizations
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+
+# setup zsh as default shell (needed logout)
+chsh -s $(which zsh)
+
+# tmux
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+
+# powerlevel9k and nerd fonts
+git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+cd
+git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh
+
+# fasd (check if current release is supported)
+sudo add-apt-repository ppa:aacebedo/fasd
+sudo apt-get update
+sudo apt-get install fasd
