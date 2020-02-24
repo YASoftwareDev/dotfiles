c1e627949e7cf667f3e3e497708af7291ee5ccff
Gaius Baltar <yasoftwaredev@gmail.com>
2020-02-24 10:59:39 +0100

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APT_PACKAGES="git vim-gtk3 curl clang build-essential cmake python3-dev python3-pip python3-venv tmux clipit zsh tig ranger jq wget man-db software-properties-common"
+APT_PACKAGES="git vim-gtk3 curl clang build-essential cmake python3-dev python3-pip python3-venv tmux clipit zsh tig ranger jq wget man-db fasd"
 
 PIP_PACKAGES="pip virtualenv"
 
@@ -13,6 +13,12 @@ pip install --upgrade $PIP_PACKAGES
 
 # Install ripgrep (grep on steroids)
 ./install-ripgrep-on-ubuntu.sh
+
+# tmux with sane configuration. You can further adjust it later with dotfiles/tmux/ files
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
 
 # fd - from Ubuntu 19.04 you can run: sudo apt install fd-find
 # but, for now:
@@ -31,7 +37,6 @@ rm fd-musl_7.4.0_amd64.deb
 # zsh and customizations
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-
 # zsh should be now default shell, if not, run below command
 # chsh -s $(which zsh)
 
@@ -42,14 +47,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 sed -i "s/%c/%d/g" ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 sed -i "s/^plugins=.*$/plugins=(git history history-substring-search dircycle dirhistory fasd vi-mode last-working-dir zsh-autosuggestions zsh-syntax-highlighting)/g" ~/.zshrc
 
-
-# tmux with sane configuration. You can further adjust it later with dotfiles/tmux/ files
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-
-
 # powerlevel10k (faster than powerlevel9k) and nerd fonts
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
@@ -58,18 +55,14 @@ git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 ./install.sh
 
-# fasd (z) (check if current release is supported)
-add-apt-repository -y ppa:aacebedo/fasd
-apt -yq update
-apt -yq install fasd
-
 #fzf (ctrl-R ctrl-T)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# now compare your ~/.zshrc with dotfiles/zsh/.zshrc and update your configuration with proper things
+echo "now compare your ~/.zshrc with dotfiles/zsh/.zshrc and update your configuration with proper things"
 
 # Below are things Vim related. It is possible that you don't want them!
-
 
 #setup vim vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -89,5 +82,4 @@ wget https://www.vim.org/scripts/download_script.php?src_id=9584
 # compiling YCM (obsolete, we have better things now, will be updated soon)
 #cd ~/.vim/bundle/YouCompleteMe;./install.py --clang-completer
 
-# now compare your ~/.zshrc with dotfiles/zsh/.zshrc and update your configuration with proper things
-echo "now compare your ~/.zshrc with dotfiles/zsh/.zshrc and update your configuration with proper things"
+
