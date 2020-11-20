6f00ffb177f6869bc4348705a0bc0664c2c7cdf2
Gaius Baltar <yasoftwaredev@gmail.com>
2020-11-20 17:23:42 +0100

    Relaxed sudo requirements for cmake and git install scripts

--- install-cmake.sh
+++ install-cmake.sh
@@ -2,14 +2,12 @@
 
 set -euo pipefail
 
-if ! [ $(id -u) = 0 ]; then
-    echo "sudo required! - run with sudoer privileges: 'sudo $0'"
-    exit 1
-fi
-
 wget -O - https://github.com/Kitware/CMake/releases/download/v3.19.0/cmake-3.19.0.tar.gz | tar xz
-cd cmake-3.*
+cd cmake-3.19.0
 
-./bootstrap
+./bootstrap --parallel=4 -- -DCMAKE_BUILD_TYPE:STRING=Release
 make -j
 sudo make install
+
+#cd ..
+#rm -rf cmake-3.19.0
